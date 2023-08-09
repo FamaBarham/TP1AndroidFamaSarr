929618d226627fcd062d6e659e73d707a24d6b59
famabarham <sfama@ept.sn>
2023-08-09 21:56:07 +0000

    first commit

---
+++ app/src/main/java/sn/ept/tp1_a_rendre/MainActivity.java
@@ -0,0 +1,24 @@
+package sn.ept.tp1_a_rendre;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
